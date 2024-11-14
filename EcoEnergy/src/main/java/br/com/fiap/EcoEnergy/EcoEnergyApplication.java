1effecee775b20d20650793522eadd402087c47c
Mmateus106 <rm98524@fiap.com.br>
2024-11-14 15:26:38 -0300

    Spring Boot Actuator implementado ao projeto

--- EcoEnergy/src/main/java/br/com/fiap/EcoEnergy/EcoEnergyApplication.java
+++ EcoEnergy/src/main/java/br/com/fiap/EcoEnergy/EcoEnergyApplication.java
@@ -2,12 +2,20 @@ package br.com.fiap.EcoEnergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@EntityScan
 @SpringBootApplication
 public class EcoEnergyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcoEnergyApplication.class, args);
+	}
+	@GetMapping(value = "/")
+	public String index(){
+		return "ECO_ENERGY";
 	}
 
 
