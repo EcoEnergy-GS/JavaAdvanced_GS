d1722bba05c2d8d91e69af432cb8d42df8e6215e
Mateus Mantovani Araújo <126684801+Mmateus106@users.noreply.github.com>
2024-11-14 13:44:21 -0300

    Arquivo do projeto gerado no Spring Initializr

---
+++ EcoEnergy/src/main/java/br/com/fiap/EcoEnergy/EcoEnergyApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.EcoEnergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoEnergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoEnergyApplication.class, args);
+	}
+
+}
---
+++ EcoEnergy/src/test/java/br/com/fiap/EcoEnergy/EcoEnergyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.EcoEnergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoEnergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
